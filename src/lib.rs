21c2cfa10dc0b387629c55f7ac74eb270e3872fa
CyberWolf <borel.loic.37@gmail.com>
2021-12-28 16:54:24 +0100

    add modfication on add_salon and add_user

--- src/lib.rs
+++ src/lib.rs
@@ -4,42 +4,50 @@ mod error;
 use crate::utils::*;
 use uuid::Uuid;
 use crate::error::Error;
-use mongodb::Client;
+use mongodb::{
+    sync::Client,
+    sync::Collection,
+    bson::doc,
+    options::FindOptions,
+};
 
 #[derive(Debug)]
 pub struct ChatManager {
-    connection: Client,
+    connection: Collection<utils::Salon>,
     list_salon: Vec<Salon>,
 }
 
 impl ChatManager {
     pub fn new(connection: Client) -> Self {
         let db = connection.database("chat");
-        let collection = db.collection::<Salon>("Salon");
-        
+        let collection_salon = db.collection::<Salon>("Salon");
+
         ChatManager {
             list_salon: Vec::new(),
-            connection: connection,
+            connection: collection_salon,
         }
     }
 
     pub fn add_salon(&mut self) -> Uuid {
         let salon = Salon::new();
-        let salon_id = Uuid::from(salon.id);
-        self.list_salon.push(salon);
+        let salon_id = salon.id;
+        self.connection.insert_one(salon,None);
+        //self.list_salon.push(salon);
 
         salon_id
     }
 
     pub fn add_user(&mut self, salon: &Uuid, user: &Uuid) -> Result<(), Error> {
-        let salon = self.list_salon.iter_mut().find(|s| s.id == *salon);
+        let filter = doc! { "&and" : [ {"id": salon.to_string()}, { "users": { "$elemMatch": { "$not" : {"&eq" : user.to_string()} } } } ] };
+        let update = doc! { "&push" : {"users" : user.to_string()}};
+        let salon_db = self.connection.update_one(filter, update, None);
 
-        match salon {
-            Some(s) => {
-                s.add_user(*user);
-                Ok(())
+        match salon_db {
+            Ok(_) => Ok(()),
+            Err(er) => {
+                eprintln!("Error during inserting user :{}",er);
+                Err(Error::MissedSalon)
             }
-            None => Err(Error::MissedSalon)
         }
     }
 
